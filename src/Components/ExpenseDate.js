1c8b0e0e5e30f2291682b466e5d37bf2b7c4b19f
saberin19 <valeriemys19@gmail.com>
2022-04-20 15:13:52 +0800

    fixed getfullyear function

--- src/Components/ExpenseDate.js
+++ src/Components/ExpenseDate.js
@@ -3,7 +3,7 @@ import "./ExpenseDate.css";
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullyear();
+  const year = props.date.getFullYear();
 
   return (
     <div classname="expense-date">
