ea359c0a101ff117d6267123d0a0c963a4ca7aca
saberin19 <valeriemys19@gmail.com>
2022-05-09 20:35:37 +0800

    added use state

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,30 +1,30 @@
 import ExpenseItem from "./Components/ExpenseItem";
-import './Expenses.css';
-import Card from '../UI/Card';
+import "./Expenses.css";
+import Card from "../UI/Card";
 
-function Expenses(props) {
-    return (
-        <Card className="expenses">
-          <ExpenseItem
-            title={props.item[0].title}
-            amount={expenses[0].amount}
-            date={expenses[0].date}
-          />
-          <ExpenseItem
-            title={expenses[1].title}
-            amount={expenses[1].amount}
-            date={expenses[1].date}
-          />
-          <ExpenseItem
-            title={expenses[2].title}
-            amount={expenses[2].amount}
-            date={expenses[2].date}
-          />
-          <ExpenseItem
-            title={expenses[3].title}
-            amount={expenses[3].amount}
-            date={expenses[3].date}
-          />
-        </Card>
-      )
-    }
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.item[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </Card>
+  );
+};
